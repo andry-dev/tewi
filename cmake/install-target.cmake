2abc505feb87269e7d66c9eeb417f1d135c6b10c
andry-dev <andry@suzunaan.me>
2019-06-07 21:03:39 +0200

    Install support, some fixes

---
+++ cmake/install-target.cmake
@@ -0,0 +1,21 @@
+include(CPack)
+
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/tewi
+    TYPE INCLUDE)
+
+if (TEWI_BUILD_DOC)
+    install(FILES ${PROJECT_BINARY_DIR}/doc/Main.pdf
+            DESTINATION doc/
+            RENAME tewidoc.pdf)
+endif()
+
+if (TEWI_BUILD_EXAMPLES)
+    file(GLOB ALL_EXAMPLES "${PROJECT_BINARY_DIR}/examples/bin/*")
+    install(PROGRAMS ${ALL_EXAMPLES}
+            DESTINATION share/examples)
+endif()
+
+install(TARGETS tewi
+        RUNTIME DESTINATION bin
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib)
